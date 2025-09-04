a03928ef2b5e75de7447f86fe11a4791a9a641ad
unknown <hl1752a@american.edu>
2025-09-03 20:32:27 -0400

    1

--- Pacman/Assets/Script/Item.cs
+++ Pacman/Assets/Script/Item.cs
@@ -2,11 +2,5 @@ using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-        }
-    }
+    
 }
